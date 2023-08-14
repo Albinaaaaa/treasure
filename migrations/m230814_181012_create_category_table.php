526d0f3ded9e557f51080b33736821af3bbee4d5
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-08-14 21:25:21 +0300

    add logic for migrations

--- migrations/m230814_181012_create_category_table.php
+++ migrations/m230814_181012_create_category_table.php
@@ -14,6 +14,7 @@ class m230814_181012_create_category_table extends Migration
     {
         $this->createTable('{{%category}}', [
             'id' => $this->primaryKey(),
+            'title'=>$this->string()
         ]);
     }
 
